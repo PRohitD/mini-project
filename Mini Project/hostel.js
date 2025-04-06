8bfa81089a9c95f7c9922fa918e1f2cdd0dd6eec
Rohit Pawar <rohitpawar9025@gmail.com>
2025-04-06 22:20:11 +0530

    Initial commit

---
+++ Mini Project/hostel.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    function bookRoom() {
+        alert("Your booking request has been received. We will contact you soon!");
+    }
+    
+    window.bookRoom = bookRoom;
+});
